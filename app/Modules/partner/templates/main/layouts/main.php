b08f1ea4a1f963d6c30ce9418ad74a6cdf2db300
rashad404 <reshad.mirzeyev@gmail.com>
2020-04-26 22:58:48 -0700

    new

--- app/Modules/partner/templates/main/layouts/main.php
+++ app/Modules/partner/templates/main/layouts/main.php
@@ -53,7 +53,7 @@ $lng->load('app');
         Url::templatePartnerPath() . 'js/scr.js',
         Url::templatePartnerPath() . 'js/script_yuel.js',
         Url::templatePartnerPath() . 'js/summernote.min.js',
-        Url::templatePartnerPath() . 'js/summernote-cleaner.js',
+//        Url::templatePartnerPath() . 'js/summernote-cleaner.js',
         Url::templatePartnerPath() . 'js/main.js',
         Url::templatePath() . 'js/main.js'.$css_v,
         Url::templatePartnerPath() . 'js/bootstrap-tagsinput.min.js',
@@ -78,23 +78,15 @@ $lng->load('app');
                     ['para', ['ul', 'ol', 'paragraph']],
                     ['table', ['table']],
                     ['insert', ['picture', 'hr']],
-                    ['view', ['fullscreen']],
                     // ['help', ['help']]
                 ],
-                cleaner:{
-                    action: 'both', // both|button|paste 'button' only cleans via toolbar button, 'paste' only clean when pasting content, both does both options.
-                    newline: '<br/>', // Summernote's default is to use '<p><br></p>'
-                    notStyle: 'position:absolute;top:0;left:0;right:0', // Position of Notification
-                    icon: '<i class="note-icon">[Your Button]</i>',
-                    keepHtml: true, // Remove all Html formats
-                    keepOnlyTags: ['<p>', '<br>', '<ul>', '<li>', '<b>', '<i>', '<strong>'], // If keepHtml is true, remove all tags except these
-                    keepClasses: false, // Remove Classes
-                    badTags: ['style', 'script', 'applet', 'embed', 'noframes', 'noscript', 'html'], // Remove full tags with contents
-                    badAttributes: ['style', 'start'], // Remove attributes from remaining tags
-                    limitChars: false, // 0/false|# 0/false disables option
-                    limitDisplay: 'both', // text|html|both
-                    limitStop: false // true/false
-                },
+                callbacks: {
+                    onPaste: function (e) {
+                        var bufferText = ((e.originalEvent || e).clipboardData || window.clipboardData).getData('Text');
+                        e.preventDefault();
+                        document.execCommand('insertText', false, bufferText);
+                    }
+                }
             });
         });
     </script>
