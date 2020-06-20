26df62757bd2914e8633106d55d78d5e3951d8af
rashad404 <rashadlive404>
2020-06-19 21:12:51 -0700

    new

--- app/Modules/partner/Controllers/News.php
+++ app/Modules/partner/Controllers/News.php
@@ -91,34 +91,61 @@ class News extends MyController{
 
 
     public function upload_image(){
+        /***************************************************
+         * Only these origins are allowed to upload images *
+         ***************************************************/
+        $accepted_origins = array("http://localhost", "http://ug.loc", "https://ug.news");
 
-// files storage folder
-        $dir = Url::uploadPath().'/redactor/images/';
-        $files = [];
-        $types = ['image/png', 'image/jpg', 'image/gif', 'image/jpeg', 'image/pjpeg'];
+        /*********************************************
+         * Change this line to set the upload folder *
+         *********************************************/
+        $imageFolder = "/Web/uploads/redactor/images";
 
-        if (isset($_FILES['file']))
-        {
-            foreach ($_FILES['file']['name'] as $key => $name)
-            {
-                $type = strtolower($_FILES['file']['type'][$key]);
-                if (in_array($type, $types))
-                {
-                    // setting file's mysterious name
-                    $filename = md5(date('YmdHis')).'.jpg';
-                    $path = $dir.$filename;
-
-                    // copying
-                    move_uploaded_file($_FILES['file']['tmp_name'][$key], $path);
-
-                    $files['file-'.$key] = array(
-                        'url' => Url::uploadPath().'/redactor/images/'.$filename.'.jpg', 'id' => $id
-                    );
+        reset ($_FILES);
+        $temp = current($_FILES);
+        if (is_uploaded_file($temp['tmp_name'])){
+            if (isset($_SERVER['HTTP_ORIGIN'])) {
+                // same-origin requests won't set an origin. If the origin is set, it must be valid.
+                if (in_array($_SERVER['HTTP_ORIGIN'], $accepted_origins)) {
+                    header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+                } else {
+                    header("HTTP/1.1 403 Origin Denied");
+                    return;
                 }
             }
+
+            /*
+              If your script needs to receive cookies, set images_upload_credentials : true in
+              the configuration and enable the following two headers.
+            */
+            // header('Access-Control-Allow-Credentials: true');
+            // header('P3P: CP="There is no P3P policy."');
+
+            // Sanitize input
+            if (preg_match("/([^\w\s\d\-_~,;:\[\]\(\).])|([\.]{2,})/", $temp['name'])) {
+                header("HTTP/1.1 400 Invalid file name.");
+                return;
+            }
+
+            // Verify extension
+            if (!in_array(strtolower(pathinfo($temp['name'], PATHINFO_EXTENSION)), array("gif", "jpg", "png"))) {
+                header("HTTP/1.1 400 Invalid extension.");
+                return;
+            }
+
+            // Accept upload if there was no origin, or if it is an accepted origin
+            $filetowrite = $imageFolder . $temp['name'];
+            move_uploaded_file($temp['tmp_name'], $filetowrite);
+
+            // Respond to the successful upload with JSON.
+            // Use a location key to specify the path to the saved image resource.
+            // { location : '/your/uploaded/image/file'}
+            echo json_encode(array('location' => $filetowrite));
+        } else {
+            // Notify editor that the upload failed
+            header("HTTP/1.1 500 Server Error");
         }
 
-        echo stripslashes(json_encode($files));
     }
 
 
