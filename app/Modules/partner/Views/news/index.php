a387a0a77db95f8198134a0ef97c9190fc46785c
rashad404 <rashadlive404>
2020-06-21 11:14:21 -0700

    new

--- app/Modules/partner/Views/news/index.php
+++ app/Modules/partner/Views/news/index.php
@@ -89,6 +89,14 @@ $defaultLang = LanguagesModel::getDefaultLanguage('partner');
                                     <td class="admin-arrow-box width-20"><?= $item["id"]?></td>
                                     <td class="admin-arrow-box">
                                         <a target="_blank" href="<?=SITE_URL?>/news/<?= $item["id"]?>/<?=Format::urlText($item['title'])?>"><?= $item["title"]?></a><br/>
+                                        <?php
+                                        if(date("d",$item['publish_time'])==date('d')){
+                                            $news_date = date("H:i",$item['publish_time']);
+                                        }else{
+                                            $news_date = date("d.m.Y H:i",$item['publish_time']);
+                                        }
+                                        ?>
+                                        <span style="color:#aaaaaa;font-size: 13px;"><?=$news_date?></span>
                                     </td>
                                     <?php $opButtons = new OperationButtons();?>
                                     <?php if($params["position"]){ ?>
