4e25264aab66680a8dfb20b78e753b14af7264ce
Rashad M <rashad.mirzaDDC.404@gmail.com>
2023-12-08 17:49:32 -0600

    fixes

--- app/Modules/partner/Views/news/index.php
+++ app/Modules/partner/Views/news/index.php
@@ -79,7 +79,28 @@ $defaultLang = LanguagesModel::getDefaultLanguage('partner');
                             </thead>
                             <tbody>
                             <?php foreach($data["list"]  as $item){ ?>
-                                
+                                <tr>
+                                    <td class="admin-arrow-box width-20">
+                                        <div class="checkboxum">
+                                            <label>
+                                                <input type="checkbox" name="row_check[]" value="<?= $item["id"]; ?>">
+                                                <span class="cr"><i class="cr-icon glyphicon glyphicon-ok"></i></span>
+                                            </label>
+                                        </div>
+                                    </td>
+                                    <td class="admin-arrow-box width-20"><?= $item["id"]?></td>
+                                    <td class="admin-arrow-box">
+                                        <a target="_blank" href="<?=SITE_URL?>/news/<?= $item["id"]?>/<?=Format::urlText($item['title'])?>"><?= $item["title"]?></a><br/>
+                                        <?php
+                                        if(date("d",$item['publish_time'])==date('d')){
+                                            $news_date = date("H:i",$item['publish_time']);
+                                        }else{
+                                            $news_date = date("d.m.Y H:i",$item['publish_time']);
+                                        }
+                                        ?>
+                                        <span style="color:#aaaaaa;font-size: 13px;"><?=$news_date?></span>
+                                    </td>
+                                </tr>
                             <?php } ?>
                             </tbody>
                         </table>
