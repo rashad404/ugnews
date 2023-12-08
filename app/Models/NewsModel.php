db93c8dfd4eec97881068fa7822a99283d53840b
Rashad M <rashad.mirzaDDC.404@gmail.com>
2023-12-08 08:36:30 -0600

    fixes

--- app/Models/NewsModel.php
+++ app/Models/NewsModel.php
@@ -109,7 +109,7 @@ class NewsModel extends Model{
     private static function isBrowserAllowed() {
         $userAgent = $_SERVER['HTTP_USER_AGENT'];
         $skipList = [
-            'Wget', 'SemrushBot', 'Barkrowler', 'AhrefsBot', 'YandexBot', 'MJ12bot', 'DotBot'
+            'Wget', 'SemrushBot', 'Barkrowler', 'AhrefsBot', 'YandexBot', 'MJ12bot', 'DotBot', 'ImagesiftBot'
         ];
 
         $pattern = '/' . implode('|', array_map('preg_quote', $skipList)) . '/i';
