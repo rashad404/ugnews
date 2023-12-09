3cc9dd6f84fe18887369742959fad32246daeaad
Rashad M <rashad.mirzaDDC.404@gmail.com>
2023-12-08 18:33:33 -0600

    fixes

---
+++ cookietest.php
@@ -0,0 +1,12 @@
+<?php
+
+// Set a cookie named "user" with the value "JohnDoe" that expires in 1 hour, available on the entire domain, and accessible via HTTP only.
+setcookie("user", "JohnDoe", time() + 3600, "/", "example.com", true, true);
+
+// Retrieve the value of the "user" cookie
+if (isset($_COOKIE["user"])) {
+    $username = $_COOKIE["user"];
+    echo "Welcome back, $username!";
+} else {
+    echo "Cookie not set.";
+}
