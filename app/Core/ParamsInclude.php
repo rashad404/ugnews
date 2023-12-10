3c9d488bc993bc6a8b546fec695f46718eec99dd
Rashad M <rashad.mirzaDDC.404@gmail.com>
2023-12-10 13:51:45 -0600

    fixes

--- app/Core/ParamsInclude.php
+++ app/Core/ParamsInclude.php
@@ -6,6 +6,6 @@
     'GOOGLE_ANALYTICS' => 'UA-163408142-1',
     'DEPOSIT_FEE' => '500',
     'APPLICATION_FEE' => '35',
-    'UPDATE_VERSION' => '3.34',
+    'UPDATE_VERSION' => '3.35',
     // 'UPDATE_VERSION' => rand(111111111,999999999),
     ];
