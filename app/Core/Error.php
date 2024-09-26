e249ca1c0169b960c1061c758d396d49e9675af2
Rashad M <rashad.mirzaDDC.404@gmail.com>
2024-09-25 23:08:32 -0500

    test

--- app/Core/Error.php
+++ app/Core/Error.php
@@ -71,7 +71,7 @@ class Error extends Controller
         View::render('error/404', $data, 'error');
     }
 	
-	public function module_index($modul='/')
+	public function module_index($modul='https://ug.news')
     {
        return  Url::redirect($modul."/error/index404");
     }
