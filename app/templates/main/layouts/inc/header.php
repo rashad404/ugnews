eb93370aaab0f0120fcf34af75d46ec572f1c873
Rashad M <rashad.mirzaDDC.404@gmail.com>
2024-09-29 22:08:32 -0500

    test

--- app/templates/main/layouts/inc/header.php
+++ app/templates/main/layouts/inc/header.php
@@ -9,8 +9,8 @@ use Models\CountryModel;
                 <a href="/" class="flex-shrink-0">
                     <img class="h-8 w-auto sm:h-10" src="<?=Url::templatePath()?>/img/partner_logos/<?=$_PARTNER['header_logo']?>" alt="<?=PROJECT_NAME?> logo"/>
                 </a>
-                <div class="hidden lg:ml-6 lg:flex lg:space-x-6">
-                    <?php foreach (array_slice($data['menus'], 0, 7) as $menu): ?>
+                <div class="hidden lg:ml-6 lg:flex lg:space-x-6 pl-4">
+                    <?php foreach (array_slice($data['menus'], 0, 5) as $menu): ?>
                         <a href="<?=$menu['url']?>" class="text-base font-medium text-gray-500 hover:text-gray-900">
                             <?=$menu['title_' . $data['def_language']]?>
                         </a>
@@ -51,24 +51,46 @@ use Models\CountryModel;
                 </div>
             </div>
             <div class="flex items-center">
-                <div x-data="{ open: false }" class="ml-3 relative">
-                    <div>
-                        <button @click="open = !open" type="button" class="flex items-center text-base font-medium text-gray-500 hover:text-gray-900" id="user-menu" aria-expanded="false" aria-haspopup="true">
-                            <i class="fas fa-globe mr-1"></i>
-                            <span class="hidden md:inline"><?=CountryModel::getCode($_SETTINGS['region'])?></span>
-                            <svg class="ml-1 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                            </svg>
-                        </button>
-                    </div>
-                    <div x-show="open" @click.away="open = false" class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
-                        <div class="px-4 py-2 text-xs text-gray-500"><?=$lng->get('Select Region')?>:</div>
-                        <?php foreach (CountryModel::getList() as $country) :?>
-                            <a href="set/region/<?=$country['id']?>" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem"><?=$country['name']?></a>
-                        <?php endforeach;?>
-                    </div>
-                </div>
-            </div>
+    <div x-data="{ open: false }" class="ml-3 relative">
+        <div>
+            <button @click="open = !open" type="button" class="flex items-center text-base font-medium text-gray-500 hover:text-gray-900" id="region-menu" aria-expanded="false" aria-haspopup="true">
+                <i class="fas fa-globe mr-1"></i>
+                <span class="hidden md:inline"><?=CountryModel::getCode($_SETTINGS['region'])?></span>
+                <svg class="ml-1 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                </svg>
+            </button>
+        </div>
+        <div x-show="open" @click.away="open = false" class="z-50 origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="region-menu">
+            <div class="px-4 py-2 text-xs text-gray-500"><?=$lng->get('Select Region')?>:</div>
+            <?php foreach (CountryModel::getList() as $country) :?>
+                <a href="set/region/<?=$country['id']?>" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem"><?=$country['name']?></a>
+            <?php endforeach;?>
+        </div>
+    </div>
+    
+    <div x-data="{ open: false }" class="ml-4 relative">
+        <div>
+            <button @click="open = !open" class="flex items-center text-base font-medium text-gray-500 hover:text-gray-900" id="user-menu" aria-expanded="false" aria-haspopup="true">
+                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+                <svg class="ml-1 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                </svg>
+            </button>
+        </div>
+        <div x-show="open" @click.away="open = false" class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
+            <?php if ($userId > 0): ?>
+                <a href="/user_panel/profile" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem"><?= $lng->get('Profile') ?></a>
+                <a href="/user_panel/logout" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem"><?= $lng->get('Logout') ?></a>
+            <?php else: ?>
+                <a href="/login" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem"><?= $lng->get('Sign in') ?></a>
+                <a href="/register" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem"><?= $lng->get('Sign up') ?></a>
+            <?php endif; ?>
+        </div>
+    </div>
+</div>
             <div class="-mr-2 -my-2 md:hidden">
                 <button @click="mobileMenuOpen = !mobileMenuOpen" type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                     <span class="sr-only">Open menu</span>
