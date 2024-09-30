0d02a48795b5abee19696d7cc7e7032effae7433
Rashad M <rashad.mirzaDDC.404@gmail.com>
2024-09-29 22:54:14 -0500

    test

--- app/templates/main/layouts/inc/footer.php
+++ app/templates/main/layouts/inc/footer.php
@@ -92,7 +92,7 @@ use Helpers\Format;
 </footer>
 
 <!-- Mobile Footer Navigation -->
-<nav class="fixed bottom-0 inset-x-0 bg-white shadow flex justify-between text-sm text-gray-700 uppercase font-mono md:hidden">
+<nav class="fixed bottom-0 inset-x-0 bg-white shadow flex justify-between text-xs text-gray-700 uppercase font-mono md:hidden">
     <a href="/" class="w-full block py-2 px-3 text-center hover:bg-gray-200">
         <svg class="w-5 h-5 mb-1 mx-auto" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
         <?=$lng->get('Home')?>
